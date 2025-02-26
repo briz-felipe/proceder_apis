f2a950f62deb22018b8cb46f65eba1ea7c0b5ca4
brizClickBus <felipe.ribeiro@clickbus.com>
2025-02-26 16:14:11 -0300

    Add dotenv for environment variable management and update database connection

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const { Sequelize, DataTypes } = require('sequelize');
 const path = require('path');
 
-const sequelize = new Sequelize('proceder', 'brizfelipe', 'XH2wYnE*', {
-    host: 'localhost',
-    dialect: 'postgres' // ou 'postgres', 'sqlite', 'mariadb', 'mssql'
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT // ou 'postgres', 'sqlite', 'mariadb', 'mssql'
 });
 
 const db = {};
